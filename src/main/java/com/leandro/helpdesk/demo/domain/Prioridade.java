9c63bbaf7ea900d0cb4fc6e450edc471032a263e
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2022-09-20 17:47:26 -0300

    Enuns

---
+++ src/main/java/com/leandro/helpdesk/demo/domain/Prioridade.java
@@ -0,0 +1,32 @@
+package com.leandro.helpdesk.demo.domain;
+
+public enum Prioridade {
+    
+    BAIXA(0, "BAIXA"), MEDIA(1, "MEDIA"), ALTA(2, "ALTA");
+
+    private Integer codigo;
+    private String descrição;
+
+    private Prioridade(Integer codigo, String descricao){
+        this.codigo = codigo;
+        this.descrição = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+    
+    public String getDescrição() {
+        return descrição;
+    }
+ 
+    public static Perfil toEnum(Integer cod){
+        if(cod == null) return null;
+
+        for(Perfil x: Perfil.values()){
+            if(cod.equals(x.getCodigo())) return x;
+        }
+
+        throw new IllegalArgumentException("Prioridade Invalida"); 
+    }
+}
---
+++ src/main/java/com/leandro/helpdesk/demo/domain/Status.java
@@ -0,0 +1,32 @@
+package com.leandro.helpdesk.demo.domain;
+
+public enum Status {
+    
+    ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), ENCERRADO(2, "ENCERRADO");
+
+    private Integer codigo;
+    private String descrição;
+
+    private Status(Integer codigo, String descricao){
+        this.codigo = codigo;
+        this.descrição = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+    
+    public String getDescrição() {
+        return descrição;
+    }
+ 
+    public static Perfil toEnum(Integer cod){
+        if(cod == null) return null;
+
+        for(Perfil x: Perfil.values()){
+            if(cod.equals(x.getCodigo())) return x;
+        }
+
+        throw new IllegalArgumentException("Status Invalido"); 
+    }
+}
